fba620ef22153cffcf73d90434901beb0a0e4387
annanyapr <34443592+annanyapr@users.noreply.github.com>
2019-01-26 00:48:11 +0530

    Update models.py

--- Blogs/models.py
+++ Blogs/models.py
@@ -8,12 +8,12 @@ class Blog(models.Model):
     title = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
     body = models.TextField()
-    posted = models.DateTimeField(db_index=True, auto_now_add=True)
+
 
     def __str__(self):
         return self.title
     def get_absolute_url(self):
-        reverse()
+        return reverse('Blogs:blogdetail', kwargs={'pk': self.id}, )
 
 
 class Comment(models.Model):
